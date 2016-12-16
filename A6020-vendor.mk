952946bf0124bb1137b0b5924947606de965b672
Prema Chand Alugu <premaca@gmail.com>
2016-12-16 23:27:57 +0530

    ims: remove some binaries that are crashing
    imscmservice and ims_rtp_daemon are crashing and lets get rid of them for now
    till we get some solution. This hasnt affected any voice calls of VoLTE.
    
    Change-Id: I9830344ac4723ac9b80e4d4e7680dacf58c7c0fa

--- A6020-vendor.mk
+++ A6020-vendor.mk
@@ -18,8 +18,6 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/a6020/proprietary/bin/adsprpcd:system/bin/adsprpcd \
     vendor/lenovo/a6020/proprietary/bin/btnvtool:system/bin/btnvtool \
     vendor/lenovo/a6020/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
-    vendor/lenovo/a6020/proprietary/bin/ims_rtp_daemon:system/bin/ims_rtp_daemon \
-    vendor/lenovo/a6020/proprietary/bin/imscmservice:system/bin/imscmservice \
     vendor/lenovo/a6020/proprietary/bin/imsdatadaemon:system/bin/imsdatadaemon \
     vendor/lenovo/a6020/proprietary/bin/imsqmidaemon:system/bin/imsqmidaemon \
     vendor/lenovo/a6020/proprietary/bin/irsc_util:system/bin/irsc_util \
