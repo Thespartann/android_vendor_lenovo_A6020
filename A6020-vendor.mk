3b6cf0adc8cdcf9a6ad62bf8b0298fa5be878152
harry8242 <mohamedharis97@gmail.com>
2017-12-07 17:48:34 +0530

    A6020: Update graphics firmware
    Change-Id: I1b0103a56aae83d31eabbe00c2c359f2ce102ff2

--- A6020-vendor.mk
+++ A6020-vendor.mk
@@ -141,8 +141,6 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/a6020/proprietary/lib64/libOmxVdecHevc.so:system/lib64/libOmxVdecHevc.so \
     vendor/lenovo/a6020/proprietary/vendor/etc/calmodule.cfg:system/vendor/etc/calmodule.cfg \
     vendor/lenovo/a6020/proprietary/vendor/etc/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf \
-    vendor/lenovo/a6020/proprietary/vendor/firmware/a420_pfp.fw:system/vendor/firmware/a420_pfp.fw \
-    vendor/lenovo/a6020/proprietary/vendor/firmware/a420_pm4.fw:system/vendor/firmware/a420_pm4.fw \
     vendor/lenovo/a6020/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
     vendor/lenovo/a6020/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:system/vendor/lib/egl/eglSubDriverAndroid.so \
     vendor/lenovo/a6020/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
